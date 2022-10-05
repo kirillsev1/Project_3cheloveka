9612abbfef25853a0049609362bd908aac73710e
radugaboost <skeal.smirnov2004@mail.ru>
2022-10-05 21:03:15 +0300

    Финальная версия доп. функций 2.0

--- sem.py
+++ sem.py
@@ -1,14 +1,6 @@
-"""File with extra functions."""
-
-
-def military_joke(n: int) -> str:
-    """This function return military joke.
-
-    Args:
-        n: int - joke number requested by the user.
-
-    Returns:
-        str: str - answer.
+def military_joke(n: int) -> str: 
+    """This function return military joke
+    n - joke number requested by the user
     """
     text = []
     file = open('military_joke.txt', 'r')
@@ -17,18 +9,13 @@ def military_joke(n: int) -> str:
     output = ''.join(text[n-1])
     if "\n" in output:
         output = output.replace('\n', '')
-    file.close()
     return output
 
+print(military_joke(3))
 
 def corona_joke(n: int) -> str:
-    """"This function return coronavirus joke.
-
-    Args:
-        n: int - joke number requested by the user.
-
-    Returns:
-        str: str - answer.
+    """"This function return coronavirus joke
+    n - joke number requested by the user
     """
     text = []
     file = open('corona_joke.txt', 'r')
@@ -37,25 +24,19 @@ def corona_joke(n: int) -> str:
     output = ''.join(text[n-1])
     if "\n" in output:
         output = output.replace('\n', '')
-    file.close()
     return output
 
-
 def cats_joke(n: int) -> str:
-    """"This function return cats joke.
-
-    Args:
-        n: int - joke number requested by the user.
-
-    Returns:
-        str - answer.
+    """"This function return cats joke
+    n - joke number requested by the user
     """
     text = []
-    file = open('cats.txt', 'r')
+    file = open('cats_joke.txt', 'r')
     for line in file.readlines():
         text.append(line)
     output = ''.join(text[n-1])
     if "\n" in output:
         output = output.replace('\n', '')
-    file.close()
     return output
+
+
