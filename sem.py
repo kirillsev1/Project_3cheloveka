e0eabcd0f7808aaefeca32b23a6c04702be5ea3c
kirillsev1 <kirillprih47@gmail.com>
2022-10-06 11:09:58 +0300

    Try

--- sem.py
+++ sem.py
@@ -5,45 +5,48 @@ def military_joke(num: int) -> str:
     """This function return military joke.
 
     Args:
-    num: int - joke number requested by the user.
+        num: int - joke number requested by the user.
+
     Returns:
-    str: str - answer.
+        str: str - answer.
     """
     with open('military_joke.txt', 'r') as anek:
         text = [line for line in anek]
-    output = ''.join(text[num - 1])
-    if "\n" in output:
-        output = output.replace('\n', '')
-    return output
+        output = ''.join(text[num - 1])
+        if "\n" in output:
+            output = output.replace('\n', '')
+        return output
 
 
 def corona_joke(num: int) -> str:
     """This function return coronavirus joke.
-    Args:
-    num: int - joke number requested by the user.
-    Returns:
-    str: str - answer.
-    """
 
+    Args:
+        num: int - joke number requested by the user.
+
+    Returns:
+        str: str - answer.
+    """
     with open('corona_joke.txt', 'r') as anek:
         text = [line for line in anek]
-    output = ''.join(text[num - 1])
-    if "\n" in output:
-        output = output.replace('\n', '')
-    return output
-
+        output = ''.join(text[num - 1])
+        if "\n" in output:
+            output = output.replace('\n', '')
+        return output
 
 
 def cats_joke(num: int) -> str:
     """This function return cats joke.
+
     Args:
-    num: int - joke number requested by the user.
+        num: int - joke number requested by the user.
+
     Returns:
-    str - answer.
+        str - answer.
     """
     with open('cats_joke.txt', 'r') as anek:
         text = [line for line in anek]
-    output = ''.join(text[num - 1])
-    if "\n" in output:
-        output = output.replace('\n', '')
-    return output
+        output = ''.join(text[num - 1])
+        if "\n" in output:
+            output = output.replace('\n', '')
+        return output
