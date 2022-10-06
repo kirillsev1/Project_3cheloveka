09dbc004ec1e4411a04ad773fa9883107f6b73ea
radugaboost <skeal.smirnov2004@mail.ru>
2022-10-06 09:31:59 +0300

    Not gig

--- sem.py
+++ sem.py
@@ -2,57 +2,46 @@
 
 
 def military_joke(num: int) -> str:
-
     """This function return military joke.
-
     Args:
     num: int - joke number requested by the user.
     Returns:
     str: str - answer.
     """
-    text = []
-    anek = open('military_joke.txt', 'r')
-    for line in anek.readlines():
-        text.append(line)
+    with open('military_joke.txt', 'r') as anek:
+        text = [line for line in anek]
     output = ''.join(text[num - 1])
     if "\n" in output:
         output = output.replace('\n', '')
     anek.close()
     return output
+
 
 def corona_joke(num: int) -> str:
-
     """This function return coronavirus joke.
-
     Args:
     num: int - joke number requested by the user.
     Returns:
     str: str - answer.
     """
-    text = []
-    anek = open('corona_joke.txt', 'r')
-    for line in anek.readlines():
-        text.append(line)
+    with open('corona_joke.txt', 'r') as anek:
+        text = [line for line in anek]
     output = ''.join(text[num - 1])
     if "\n" in output:
         output = output.replace('\n', '')
-    anek.close()
     return output
+
 
 def cats_joke(num: int) -> str:
     """This function return cats joke.
-
     Args:
     num: int - joke number requested by the user.
     Returns:
     str - answer.
     """
-    text = []
-    anek = open('cats_joke.txt', 'r')
-    for line in anek.readlines():
-        text.append(line)
+    with open('cats_joke.txt', 'r') as anek:
+        text = [line for line in anek]
     output = ''.join(text[num - 1])
     if "\n" in output:
         output = output.replace('\n', '')
-    anek.close()
     return output
