cb84383e25f29398714b6e3172e605c1e0f15d18
kirillsev1 <kirillprih47@gmail.com>
2022-10-14 05:12:12 +0300

    new input

--- main.py
+++ main.py
@@ -12,24 +12,25 @@ def main(number: int) -> str:
          str - answer.
     """
     try:
-        out = int(input('Введите номер анекдота: '))
+        files = ['military_joke', 'corona_joke', 'cats_joke']
+        out = int(input('You have chosen a topic {0}. Choose a joke number from 1 to 3: \n'.format(files[number-1])))
     except ValueError:
         return 'ValueError'
     with open('military_joke.txt', 'r') as fir:
-        if number == 1 and 1 <= out <= len(fir.readlines()):
+        if number == 1 and 1 <= out < len(fir.readlines()):
             return military_joke(out)
     with open('corona_joke.txt', 'r') as sec:
-        if number == 2 and 1 <= out <= len(sec.readlines()):
+        if number == 2 and 1 <= out < len(sec.readlines()):
             return cats_joke(out)
     with open('cats.txt', 'r') as thi:
-        if number == 3 and 1 <= out <= len(thi.readlines()):
+        if number == 3 and 1 <= out < len(thi.readlines()):
             return corona_joke(out)
     return 'Try again'
 
 
 if __name__ == '__main__':
     while True:
-        inp = input('Input number from 1 to 3. To quit input Q: ')
+        inp = input('1 - military jokes\n2 - corona jokes\n3 - cat jokes\nInput number from 1 to 3. To quit input q: \n')
         if inp == 'Q':
             break
         try:
